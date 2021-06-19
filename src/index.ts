8af6f68347dd8e7c4c4ffc3c11bccc99a34bcc69
ntasicc <nikols.tt@elfak.rs>
2021-06-19 22:47:30 +0200

    Button drawing moved to class

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,8 @@ const view1: FightSimulatorView = new FightSimulatorView()
 
 view1.drawView()
 
-let buttonFight = document.createElement('button')
-buttonFight.className = 'FIGHT'
-buttonFight.innerHTML = 'FIGHT'
-buttonFight.disabled = true
-document.body.appendChild(buttonFight)
-
 const btn: FightButton = new FightButton(
   view.fighterIsReady,
   view1.fighterIsReady
 )
-btn.readyToFight()
+btn.drawButton()
